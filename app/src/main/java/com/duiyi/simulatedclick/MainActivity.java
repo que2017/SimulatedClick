d65779a690e48aa6b6d56074ec0b3791f32e9b02
que2017 <1563645183@qq.com>
2019-12-15 21:21:32 +0800

    增加跳转辅助功能界面

--- app/src/main/java/com/duiyi/simulatedclick/MainActivity.java
+++ app/src/main/java/com/duiyi/simulatedclick/MainActivity.java
@@ -1,13 +1,24 @@
 package com.duiyi.simulatedclick;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 
 public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        try{
+            startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+        } catch (Exception e) {
+            Log.i(TAG, "start ACTION_ACCESSIBILITY_SETTINGS fail: " + e.getMessage());
+            startActivity(new Intent(Settings.ACTION_SETTINGS));
+        } finally {
+            finish();
+        }
     }
 }
