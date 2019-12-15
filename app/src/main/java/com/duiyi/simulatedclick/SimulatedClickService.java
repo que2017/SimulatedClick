ef7c780139a0b4f6660b7aa0f2d9d818b8b350e4
que2017 <1563645183@qq.com>
2019-12-15 21:10:22 +0800

    完成模拟点击通知功能

--- app/src/main/java/com/duiyi/simulatedclick/SimulatedClickService.java
+++ app/src/main/java/com/duiyi/simulatedclick/SimulatedClickService.java
@@ -1,12 +1,16 @@
 package com.duiyi.simulatedclick;
 
 import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.GestureDescription;
+import android.annotation.TargetApi;
+import android.graphics.Path;
 import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.annotation.RequiresApi;
+import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
-import android.view.accessibility.AccessibilityNodeInfo;
-
-import java.util.List;
 
 /**
  * 模拟点击的服务类
@@ -17,6 +21,14 @@ import java.util.List;
 public class SimulatedClickService extends AccessibilityService {
     private static final String TAG = SimulatedClickService.class.getSimpleName();
 
+    private Handler handler = new Handler(Looper.getMainLooper()) {
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public void handleMessage(Message msg) {
+            dispatchGestureClick(160, 60);
+        }
+    };
+
     /**
      * 监听窗口变化的回调
      *
@@ -26,18 +38,24 @@ public class SimulatedClickService extends AccessibilityService {
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
         int eventType = event.getEventType();
+        Log.i(TAG, "onAccessibilityEvent: " + eventType);
         switch (eventType) {
             // 通知栏发生变化
             case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
-                // 模拟点击
-                AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
-                List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByText("信息");
-                for (AccessibilityNodeInfo msgNodeInfo : list) {
-                    msgNodeInfo.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                }
+                // 模拟点击通知栏，因为通知栏弹出有动画，所以需要延迟
+                handler.sendEmptyMessageDelayed(0, 1500);
                 break;
             default:
         }
+    }
+    @RequiresApi(24)
+    public void dispatchGestureClick(int x, int y) {
+        Path path = new Path();
+        path.moveTo(x, y);
+        boolean click = dispatchGesture(new GestureDescription
+                .Builder()
+                .addStroke(new GestureDescription.StrokeDescription(path, 0, 100)).build(),null, null);
+        Log.i(TAG, "dispatchGestureClick: " + click);
     }
 
     /**
